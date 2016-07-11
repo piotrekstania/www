efc2ce2a4ac2baca5523ce3038197ff8c7d58ab3
piotrekstania <piotrekstania@gmail.com>
2016-07-11 10:39:23 +0200

    +

---
+++ RtUsers.php
@@ -0,0 +1,260 @@
+<?php
+
+class RtUsers {
+
+	private $db_dir          = "data.db";
+	private $db_table_users  = "users";
+	private $db_admin_user   = "admin";
+	private $db_admin_pass   = "admin";
+
+	private $session_time = 60;
+
+	private $message;
+	private $user_name;
+
+	function __construct() {
+
+		session_start();
+
+		$this->message = "Użytkownik niezalogowny.";
+
+		//brak inicjalizacji -> wygenerowanie nowgo id
+		if (!isset($_SESSION['init'])) {
+			session_regenerate_id();
+			$_SESSION['init'] = true;
+			$_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
+		}
+
+		//niezgodnosc ip, automatyczne wylogowanie
+		if($_SESSION['ip'] != $_SERVER['REMOTE_ADDR']) {
+			$_SESSION = array();
+			$this->message = "Niezgodność IP.";
+			return;
+		}
+
+		//pierwsza wizyta niezalogowanego uzytkownia
+		if(!isset($_SESSION['id'])) {
+			$_SESSION['id'] = 0;
+		}
+
+		//czas sesji
+		if($_SESSION['id'] > 0) {
+			if($_SESSION['time'] < (time()-$this->session_time)) {
+				$_SESSION = array();
+				$this->message = "Sesja wygasła.";
+				return;
+			} else $_SESSION['time'] = time();
+		}
+
+		try {
+			//otwarcie bazy i ustawienie parametrow
+			$this->db = new PDO('sqlite:' . $this->db_dir);
+			$this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+			$this->db->setAttribute(PDO::ATTR_TIMEOUT, 5);
+
+			//sprawdzenie czy istnieje tabela z uzytkownikami
+			$sql = sprintf("SELECT name FROM sqlite_master WHERE type='table' AND name='%s'", $this->db_table_users);
+
+			//jezeli nie ma tabeli z uzytkownikami
+			if($this->db->query($sql)->fetch() == null) {
+
+				//utworzenie nowej tabeli
+				$sql = sprintf("CREATE TABLE '%s' (
+					'id'	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+					'user'	TEXT NOT NULL UNIQUE,
+					'pass'	TEXT NOT NULL);", $this->db_table_users);
+				$this->db->exec($sql);
+			}
+
+			//sprawdzenie czy istnieje konto admina z indexsem 1
+			$sql = sprintf("SELECT * FROM '%s' WHERE id=1 AND user='%s'", $this->db_table_users, $this->db_admin_user);
+
+			if($this->db->query($sql)->fetch() == null) {
+
+				//dodanie admina do tabeli
+				$sql = sprintf("REPLACE INTO %s(id, user, pass) VALUES(1, '%s', '%s')",
+					$this->db_table_users,
+					$this->db_admin_user,
+					password_hash($this->db_admin_pass, PASSWORD_DEFAULT));
+				$this->db->exec($sql);
+			}
+
+		} catch(PDOException $e) {
+			$this->message = $e->getMessage();
+			return;
+		}
+
+		if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+			switch($_POST['type']) {
+
+				case 'logout':
+					$_SESSION = array();
+					$this->message = "Poprawnie wylogowano.";
+					return;
+
+				case 'login':
+					try {
+						$sql = sprintf("SELECT * FROM %s WHERE user='%s'", $this->db_table_users, $_POST['user']);
+						$result = $this->db->query($sql)->fetch();
+					} catch(PDOException $e) {
+						$this->message = $e->getMessage();
+						return;
+					}
+
+					if(password_verify($_POST['pass'], $result['pass'])) {
+						session_regenerate_id();
+						$_SESSION['init'] = true;
+						$_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
+						$_SESSION['id'] = $result['id'];
+						$_SESSION['time'] = time();
+					} else {
+						$this->message = "Niepoprawne dane.";
+						return;
+					}
+					break;
+
+				case 'change_pass':
+					if($_SESSION['id'] == 0) {
+						$this->message = "Brak uprawnień.";
+						return;
+					}
+
+					try {
+						$sql = sprintf("SELECT * FROM %s WHERE id=%u", $this->db_table_users, $_SESSION['id']);
+						$result = $this->db->query($sql)->fetch();
+
+						if(!password_verify($_POST['old_pass'], $result['pass']) || ($_POST['new_pass_1'] != $_POST['new_pass_2'])) {
+							$this->message = "Niepoprawne dane.";
+							return;
+						}
+
+						$sql = sprintf("UPDATE %s SET pass='%s' WHERE id=%u", $this->db_table_users, password_hash($_POST['new_pass_1'], PASSWORD_DEFAULT), $_SESSION['id']);
+
+						if($this->db->exec($sql)) $this->message = "Hasło zostało zmienione.";
+						else $this->message = "Niepoprawne dane.";
+						return;
+
+					} catch(PDOException $e) {
+						$this->message = $e->getMessage();
+						return;
+					}
+					break;
+
+				//dodawanie nowego uzytkownika
+				case 'add_user':
+					if($_SESSION['id'] != 1) {
+						$this->message = "Brak uprawnień.";
+						return;
+					}
+
+					if(empty($_POST['user']) || empty($_POST['pass'])) {
+						$this->message = "Niepoprawne dane.";
+						return;
+					}
+
+					try {
+						$sql = sprintf("INSERT INTO %s(user, pass) VALUES('%s', '%s')",
+							$this->db_table_users,
+							$_POST['user'],
+							password_hash($_POST['pass'], PASSWORD_DEFAULT));
+
+						if($this->db->exec($sql)) $this->message = "Dodano nowego użytkownika.";
+						else $this->message = "Niepoprawne dane.";
+						return;
+
+					} catch(PDOException $e) {
+						$this->message = $e->getMessage();
+						return;
+					}
+					break;
+
+					//usuwanie uzytkownika
+					case 'remove_user':
+						if($_SESSION['id'] != 1) {
+							$this->message = "Brak uprawnień.";
+							return;
+						}
+
+						if(empty($_POST['id']) || ($_POST['id'] < 2)) {
+							$this->message = "Niepoprawne dane.";
+							return;
+						}
+
+						try {
+							$sql = sprintf("DELETE FROM %s WHERE id=%u",
+								$this->db_table_users,
+								$_POST['id']);
+
+							if($this->db->exec($sql)) $this->message = "Usunięto użytkownika.";
+							else $this->message = "Niepoprawne dane.";
+							return;
+
+						} catch(PDOException $e) {
+							$this->message = $e->getMessage();
+							return;
+						}
+						break;
+
+				}
+			}
+
+			if($_SESSION['id'] > 0) $this->message = "Użytkownik zalogowny.";
+		}
+
+
+	public function isLogin() {
+		if(isset($_SESSION['id'])) {
+			if($_SESSION['id'] > 0) return true;
+		}
+		return false;
+	}
+
+
+	public function isAdmin() {
+		if(isset($_SESSION['id'])) {
+			if($_SESSION['id'] == 1) return true;
+		}
+		return false;
+	}
+
+	public function getMessage() {
+		return $this->message;
+	}
+
+	public function getUserName() {
+
+		if(!isset($_SESSION['id'])) return false;
+		if($_SESSION['id'] == 0) return false;
+
+		try {
+			$sql = sprintf("SELECT user FROM %s WHERE id=%s", $this->db_table_users, $_SESSION['id']);
+			$result = $this->db->query($sql)->fetch();
+			if($result) return $result['user'];
+		} catch(PDOException $e) {
+			$this->message = $e->getMessage();
+			return false;
+		}
+
+		return false;
+	}
+
+	public function getUsers() {
+		if(!isset($_SESSION['id'])) return false;
+		if($_SESSION['id'] != 1) return false;
+
+		try {
+			$sql = sprintf("SELECT id, user FROM %s", $this->db_table_users);
+			$result = $this->db->query($sql)->fetchAll();
+			if($result) return $result;
+		} catch(PDOException $e) {
+			$this->message = $e->getMessage();
+			return false;
+		}
+
+		return false;
+	}
+
+}
+?>
--- index.php
+++ index.php
@@ -1,48 +1,84 @@
 <?php
 
-require('RntUsers.php');
+function build_table($array){
+    // start table
+    $html = '<table>';
+    // header row
+    $html .= '<tr>';
+    foreach($array[0] as $key=>$value){
+            $html .= '<th>' . $key . '</th>';
+        }
+    $html .= '</tr>';
 
+    // data rows
+    foreach( $array as $key=>$value){
+        $html .= '<tr>';
+        foreach($value as $key2=>$value2){
+            $html .= '<td>' . $value2 . '</td>';
+        }
+        $html .= '</tr>';
+    }
 
-try {
-	$s = new RntUsers();
+    // finish table and return it
 
-	if($s->isLogin()) {
-		$form = '<form action="index.php" method="post">
-							<input type="hidden" name="type" value="logout">
-		  				<button type="submit">wyloguj</button>
-						</form>';
-
-			$form .= '<br>
-				<form action="index.php" method="post">
-					<input type="hidden" name="type" value="adduser">
-					user: <input type="text" name="user"><br>
-			  	pass: <input type="text" name="pass"><br>
-					session_time: <input type="number" name="session_time"><br>
-			  	<button type="submit">dodaj</button>
-			</form>
-				<br>';
-
-			} else {
-
-			$form = '
-				<form action="index.php" method="post">
-					<input type="hidden" name="type" value="login">
-					user: <input type="text" name="user"><br>
-					pass: <input type="text" name="pass"><br>
-					<button type="submit">zaloguj</button>
-			</form>
-				';
-
-
-
-			}
-
-
-			echo "<br>" . $s->getMessage() . '<br>';
-			echo $form;
-
-
-}catch(Exception $e) {
-	echo $e->getMessage();
+    $html .= '</table>';
+    return $html;
 }
+
+require('RtUsers.php');
+
+$s = new RtUsers();
+
+if($s->isLogin()) {
+	$form = '<form action="index.php" method="post">
+						<input type="hidden" name="type" value="logout">
+	  				<button type="submit">wyloguj</button>
+					</form>';
+
+	$form .= '<br>';
+
+	$form .= '<form action="index.php" method="post">
+						<input type="hidden" name="type" value="change_pass">
+						old: <input type="text" name="old_pass"><br>
+						new1: <input type="password" name="new_pass_1"><br>
+						new2: <input type="password" name="new_pass_2"><br>
+	  				<button type="submit">zmień</button>
+					</form>';
+
+					if($s->isAdmin()){
+						$form .= '<br>
+							<form action="index.php" method="post">
+								<input type="hidden" name="type" value="add_user">
+								user: <input type="text" name="user"><br>
+						  	pass: <input type="text" name="pass"><br>
+						  	<button type="submit">dodaj</button>
+						</form><br>';
+
+						$form .= build_table($s->getUsers());
+
+						$form .= '<br>
+							<form action="index.php" method="post">
+								<input type="hidden" name="type" value="remove_user">
+								id: <input type="text" name="id"><br>
+						  	<button type="submit">usuń</button>
+						</form><br>';
+
+					}
+
+
+		} else {
+		$form = '
+			<form action="index.php" method="post">
+				<input type="hidden" name="type" value="login">
+				user: <input type="text" name="user"><br>
+				pass: <input type="password" name="pass"><br>
+				<button type="submit">zaloguj</button>
+		</form>';
+		}
+
+
+		echo "<br>" . $s->getMessage() . '<br>';
+		echo "zalogowany jako " . $s->getUserName() . "<br>";
+		echo $form;
+
 ?>
