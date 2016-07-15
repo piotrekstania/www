a5d81f79bf10110abc3ca08bf7c70a26c7783fae
piotrekstania <piotrekstania@gmail.com>
2016-07-15 12:08:29 +0200

    +

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 function build_table($array){
     // start table
     $html = '<table style="text-align: end;">';
@@ -82,75 +82,121 @@ if($s->isLogin()) {
 		echo $s->getMessage() . '<br><br>';
 		echo "<a href='index.php'>Index</a><br>";
 		echo $form;
+*/
 
+require('RtUsers.php');
 
+$users = new RtUsers();
 ?>
 
 <!doctype html>
-<html>
+<!--
+  Material Design Lite
+  Copyright 2015 Google Inc. All rights reserved.
 
-<head>
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
 
-	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-	<link rel="stylesheet" href="https://code.getmdl.io/1.1.3/material.indigo-pink.min.css">
-	<script defer src="https://code.getmdl.io/1.1.3/material.min.js"></script>
+      https://www.apache.org/licenses/LICENSE-2.0
 
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License
+-->
+<html lang="pl">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Panel HPS521">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+    <title>Otech - HPS521</title>
 
-</head>
+    <link rel="shortcut icon" href="images/favicon.png">
 
-<body>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.1.3/material.cyan-light_blue.min.css">
+    <link rel="stylesheet" href="styles.css">
+  </head>
 
-	<div class="mdl-grid main-grid" id="main-grid">
+	<body>
+	<div class="demo-layout mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
 
+		<header class="demo-header mdl-layout__header mdl-color--grey-100 mdl-color-text--grey-600">
+			<div class="mdl-layout__header-row">
+				<span class="mdl-layout-title">Home</span>
+				<div class="mdl-layout-spacer"></div>
 
+				<button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" id="hdrbtn">
+					<i class="material-icons">more_vert</i>
+				</button>
 
-		<div class="mdl-card mdl-shadow--6dp mdl-cell mdl-cell--4-col card">
-
-			<div class="mdl-card__title" style="background-color:blue;">
-				<h2 class="mdl-card__title-text">Zmiana hasła</h2>
-			</div>
-
-			<div class="mdl-card__supporting-text card-supporting-text">
-
-				<form action="index.php" method="post">
-
-					<input type="hidden" name="type" value="change_pass">
-
-					<table class="mdl-data-table mdl-js-data-tabl">
-  					<tbody>
-    					<tr>
-      					<td class="mdl-data-table__cell--non-numeric">Hasło</td>
-      					<td style="width:100%;"><input type="password" name="old_pass"></td>
-    					</tr>
-
-							<tr>
-      					<td class="mdl-data-table__cell--non-numeric">Nowe hasło</td>
-      					<td><input type="password" name="new_pass"></td>
-    					</tr>
-
-							<tr>
-      					<td class="mdl-data-table__cell--non-numeric">Potwierdź hasło</td>
-      					<td><input type="password" name="new_pass1"></td>
-    					</tr>
-
-  					</tbody>
-					</table>
-
-
-					<button class="mdl-button mdl-js-button mdl-button--raised mdl-button--accent" type="submit" style="float:right; margin-top:20px;">Zmień hasło</button>
-
-				</form>
+				<ul class="mdl-menu mdl-js-menu mdl-js-ripple-effect mdl-menu--bottom-right" for="hdrbtn">
+					<li class="mdl-menu__item">About</li>
+					<li class="mdl-menu__item">Contact</li>
+					<li class="mdl-menu__item">Legal information</li>
+				</ul>
 
 			</div>
+		</header>
 
+
+
+
+
+
+		<div class="demo-drawer mdl-layout__drawer mdl-color--blue-grey-900 mdl-color-text--blue-grey-50">
+
+			<header class="demo-drawer-header">
+
+
+					<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+	    			<input class="mdl-textfield__input" type="text" id="user">
+	    			<label class="mdl-textfield__label" for="user">Login</label>
+	  			</div>
+
+					<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+	    			<input class="mdl-textfield__input" type="password" id="pass">
+	    			<label class="mdl-textfield__label" for="pass">Hasło</label>
+	  			</div>
+
+
+			</header>
+
+
+			<nav class="demo-navigation mdl-navigation mdl-color--blue-grey-800">
+				<a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">home</i>Home</a>
+				<a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">inbox</i>Inbox</a>
+				<a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">delete</i>Trash</a>
+				<a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">report</i>Spam</a>
+				<a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">forum</i>Forums</a>
+				<a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">flag</i>Updates</a>
+				<a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">local_offer</i>Promos</a>
+				<a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">shopping_cart</i>Purchases</a>
+				<a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">people</i>Social</a>
+				<div class="mdl-layout-spacer"></div>
+				<a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">help_outline</i><span class="visuallyhidden">Help</span></a>
+			</nav>
 		</div>
 
 
 
+
+
+
+		<main class="mdl-layout__content mdl-color--grey-100">
+
+			<div class="mdl-grid demo-content">
+
+			</div>
+		</main>
+
 	</div>
 
 
-
-</body>
-
+		<script src="https://code.getmdl.io/1.1.3/material.min.js"></script>
+	</body>
 </html>
